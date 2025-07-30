16b02fbbba91c3dd37b2608a8e376d14e8022df0
Spencer-C-Peters <spencer.peters7987@gmail.com>
2025-07-30 15:29:43 +0200

    Fixed Sever class

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
-const videoGameRoutes = require('/routes/videoGame');
+const videoGameRoutes = require('./routes/videoGame');
 
 //Express app
 const app = express();
+
+//Middleware
+app.use(express.json())
 
 //Logs request that are made
 //Moves on to the next request only if it is used.
@@ -14,7 +17,7 @@ app.use((req, res, next) => {
 })
 
 //Calls all the routes from the file in the routes folder and runs them when called.
-app.use('/api.videogames', videoGameRoutes)
+app.use('/api/videogames', videoGameRoutes)
 
 //Listen for requests
 app.listen(process.env.PORT, () => {
